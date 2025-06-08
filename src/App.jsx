ac1c9e214cb34df27eee541fce3a259b9df0dcbd
mohitkhatri05 <mohitkhatri.86005@gmai.com>
2025-06-08 19:39:05 +0530

    name fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
